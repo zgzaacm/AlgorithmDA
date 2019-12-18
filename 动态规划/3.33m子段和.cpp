8d7cf5dc196033ce6146e4d019e9b0222150b23d
zgzaacm <1050743651@qq.com>
2019-12-19 00:04:54 +0800

    debug

--- 动态规划/3.33m子段和.cpp
+++ 动态规划/3.33m子段和.cpp
@@ -2,21 +2,24 @@
 #include<cstring>
 #include<algorithm>
 using namespace std;
-int a[100],m,M[100],dp[10][100];
+int a[100],m,n,M[100],dp[100];//默认置零 
 int main(){
 	cin>>m;
 	cin>>n;
-	for(int i=0;i<n;i++)cin>>a[i];
-	for(int i=0;i<m;i++)dp[i][0]=0;
-	for(int i=0;i<n;i++)dp[0][i]=0;
-	memset(M,M+n,0);
-	for(int i=1;i<=m;i++)
-		for(int j=i;j<=n-(m-i);j++)
-		//j>i一定要加  
-		if(j>i){
-			dp[i][j]=max(dp[i][j-1]+a[j],M[j]+a[j]);
-			M[j]=max(M[j-1],dp[i][j]);
+	for(int i=1;i<=n;i++)cin>>a[i];
+	for(int i=1;i<=m;i++){
+		dp[i]=dp[i-1]+a[i];
+		//M[i-1]=dp[i] 咱也不知道这句代码是干啥的 
+		int maxx=dp[i];
+		for(int j=i+1;j<=n-m+i;j++){
+			dp[j]=max(dp[j-1]+a[j],M[j-1]+a[j]);
+			M[j-1]=maxx;
+			maxx=max(maxx,dp[j]);
 		}
-	cout<<dp[m][n];
+		M[i+n-m]=maxx;
+	}
+	int sum=0;
+	for(int i=m;i<=n;i++)sum=max(sum,dp[i]);
+	cout<<sum;
 	return 0;
 }
